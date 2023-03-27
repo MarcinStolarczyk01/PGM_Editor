52366964c81d346293563a8f034ab6c80ecd1338
Marcin <m.stolar2001@gmail.com>
2023-03-27 13:50:26 +0200

    pododawalem komentarze

--- editor.h
+++ editor.h
@@ -12,8 +12,8 @@ protected:
 	void Negative();
 	void Reverse();
 	void Turn90();
-	void Noise(int _noise_lvl);
+	void Noise(int _noise_lvl); //it changes random pixels to the lightest value (noised image can be filtered but a lot of data will be lost)
 	void Filter();
-	void ChangeProportions(char _choice);
-	friend class Repository;
+	void ChangeProportions(char _choice);//formats like 1:1 16:9 and 4:3, the method cuts sides
+	friend class Repository; //editor isn't free so we need to buy permission and "making friens" :)
 };
